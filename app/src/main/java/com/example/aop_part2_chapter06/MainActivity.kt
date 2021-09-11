e9cceedf0552726ebda7d26f268538713ecbd96a
Sohee <sohee3480@gmail.com>
2021-09-11 17:48:52 +0900

    seekBar 설정하기  - progress만 구현함

--- app/src/main/java/com/example/aop_part2_chapter06/MainActivity.kt
+++ app/src/main/java/com/example/aop_part2_chapter06/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.aop_part2_chapter06
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.SeekBar
@@ -22,5 +23,29 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bindViews()
+    }
+
+    private fun bindViews() {
+        seekBar.setOnSeekBarChangeListener(
+            object : SeekBar.OnSeekBarChangeListener {
+                @SuppressLint("SetTextI18n")
+                override fun onProgressChanged(
+                    seekBar: SeekBar?,
+                    progress: Int,
+                    fromUser: Boolean
+                ) {
+                    minutesTxt.text = "%02d".format(progress)
+                }
+
+                override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                }
+
+                override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                }
+
+            }
+        )
     }
 }

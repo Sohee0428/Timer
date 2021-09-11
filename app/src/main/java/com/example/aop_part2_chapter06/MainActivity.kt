94a708df0f1d0d71b278b73dc1c4cb3d9324bd76
Sohee <sohee3480@gmail.com>
2021-09-11 17:47:45 +0900

    id에 따른 변수 저장하기

--- app/src/main/java/com/example/aop_part2_chapter06/MainActivity.kt
+++ app/src/main/java/com/example/aop_part2_chapter06/MainActivity.kt
@@ -2,8 +2,23 @@ package com.example.aop_part2_chapter06
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private val minutesTxt: TextView by lazy {
+        findViewById(R.id.minutesTxt)
+    }
+
+    private val secondsTxt: TextView by lazy {
+        findViewById(R.id.secondsTxt)
+    }
+
+    private val seekBar: SeekBar by lazy {
+        findViewById(R.id.seekBar)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
